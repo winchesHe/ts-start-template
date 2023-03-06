fa8226c0e94e21ca2638dd1e4c36e0ec77a779ee
yang1206 <yang15188280673@outlook.com>
2023-03-06 14:37:09 +0800

    feat: add vue,react,uniapp template

---
+++ template/react/src/routers/modules/index.tsx
@@ -0,0 +1,14 @@
+import { lazy } from 'react'
+import type { RouteObject } from '@/routers/interface'
+import lazyLoad from '@/routers/utils/lazyLoad'
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: lazyLoad(lazy(() => import('@/pages/Home'))),
+  },
+  {
+    path: '/count',
+    element: lazyLoad(lazy(() => import('@/pages/Count'))),
+  },
+]
+export default routes
---
+++ template/uniapp/src/pages/count/count.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { useCounterStore } from '@/store'
+const useCount = useCounterStore()
+const { inc, dec } = useCount
+</script>
+
+<template>
+  <div px-4 py-10 text-center font-sans text-gray-700 class="dark:text-gray-200">
+    <p class="font-semibold">
+      {{ useCount.count }}
+    </p>
+    <button class="m-4 h-8 w-8 rounded-full text-center lh-8 btn" @click="inc()">
+      +
+    </button>
+    <button class="m-4 h-8 w-8 rounded-full text-center lh-8 btn" @click="dec()">
+      -
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
